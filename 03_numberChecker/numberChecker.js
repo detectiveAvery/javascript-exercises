46ced2a77dbc75e68ae6d266bd6abf6ed1862681
detectiveAvery <izazhaidermail@gmail.com>
2025-09-20 09:31:23 +0500

    fix(numberChecker): Fix the numberChecker logic

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,11 +1,7 @@
 function numberChecker(number) {
-  if (number === 1000) {
+  if (number >= 10) {
     return true;
-  } else if (number === 10) {
-    return true;
-  } else if (number === 9) {
-    return false;
-  } else if (number === 6) {
+  } else {
     return false;
   }
 }
