00d87b4890bb6f69962ccaea4a504de737984580
avery <izazhaidermail@gmail.com>
2025-10-14 20:35:39 +0500

    fix(repeatString): Return a repeating string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let stringRepeat = [];
+  if (num < 0) {
+    return 'ERROR';
+  }
+  for (let i = num; i > 0; i--) {
+    stringRepeat.push(string);
+  }
+  return stringRepeat.join("");
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+console.log(repeatString('hey', 3));
