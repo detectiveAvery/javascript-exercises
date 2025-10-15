68b2f4902e7a3471028d88f0d9aef4c571630681
avery <izazhaidermail@gmail.com>
2025-10-15 21:47:52 +0500

    fix(removeFromArray): Remove values from an array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementToRemove) {
+  return array.filter((val) => !elementToRemove.includes(val));
 };
 
 // Do not edit below this line
